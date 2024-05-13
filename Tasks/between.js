f58393b155c0dae7d3c56eaf049454392565debb
DimaFomenko <141549473+DimaFomenko@users.noreply.github.com>
2024-05-13 22:03:13 +0200

    Update between.js by Dima

--- Tasks/between.js
+++ Tasks/between.js
@@ -1,21 +1,23 @@
-// Extract substring between prefix and suffix
 
-getvaluebetween = (str, p, s) => {
-  i = str.indexOf(p);
-  if (i === -1) return '';
-  else {
-    k = i + p.length;
-    str = str.substring(k);
-    if (s) {
-      i = str.indexOf(s);
-      if (i === -1) {
-        return '';
-      } else {
-        str = str.substring(0, i);
-      }
-    }
+  function myF (str,x) {                          // function for cut string and return "clean" part
+    const y = str.indexOf(x);          // find index of 'x'
+    if (y === -1) return null;         // or return as null
+    const z = str.slice(y);            // 'trimming' 
+    return z;
   }
-  return str;
-};
+    
+  const getValueBetween = (str, p, s) => {
+    const i = myF(str,p);                         //delete prefix
+    if (i === null) return 'prefix not fount';
+    
+    const revI = i.split('').reverse().join('');  // prepare string 'i' for function 'myF'
+    const revS = s.split('').reverse().join('');  // tha same thing
+    
+    const result = myF(revI,revS);                // delete suffix
+    if (result === null) return 'suffix not found';
+  
+    const finalResult = result.split('').reverse().join('');  // tern back string to the correct order
+    return finalResult;
+  };
 
 require('../Tests/between.js')(getvaluebetween);
